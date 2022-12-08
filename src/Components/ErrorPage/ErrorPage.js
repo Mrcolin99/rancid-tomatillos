6e8f1252af30e5b3c1caaf7a3a7cc7130d13dad5
Connor Corcoran <connorcorc@gmail.com>
2022-12-08 12:59:54 -0700

    Format movie data

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -3,7 +3,7 @@ import './ErrorPage.css'
 const ErrorPage = () => {
   return (
     <div className="error">
-      <h1>Something went wrong! <br /> It's probably our fault</h1>
+      <h1>Something went wrong! <br /> It's probably our fault <br/ > Or Heroku</h1>
     </div>
   )
 }
